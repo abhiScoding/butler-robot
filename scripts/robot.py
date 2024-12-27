6fd1ad3d8564e2c579931d2d60040de57464ee7d
abhiScoding <abhisekp2301@gmail.com>
2024-12-27 16:18:52 +0530

    robot moves to kitchen when the order received

--- scripts/robot.py
+++ scripts/robot.py
@@ -3,11 +3,13 @@
 import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+from std_msgs.msg import String
 import math
 
 vel = Twist()
+message = ""
 
-def callback(odom):
+def callback1(odom):
     global x, y, theta
     x = odom.pose.pose.position.x
     y = odom.pose.pose.position.y
@@ -15,10 +17,17 @@ def callback(odom):
     # print(round(x, 2), round(y, 2))
     return 0
 
+def callback2(data):
+    global message
+    message = data.data
+    # print(message)
+    return 0
+
 
 def robot():
     rospy.init_node('robot')
-    rospy.Subscriber('/odom', Odometry, callback)
+    rospy.Subscriber('/odom', Odometry, callback1)
+    rospy.Subscriber('chatter', String, callback2)
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
     rospy. sleep(1)
@@ -27,24 +36,25 @@ def robot():
     robotAngle = math.asin(theta)
     angleDiff = math.atan((goaly - y) / (goalx- x)) - 2*robotAngle
     linear = 0.5*math.sqrt((goalx - x)**2 + (goaly - y)**2)
+    print("Waiting for the orders!")
     while not rospy.is_shutdown():
+        if message.lower() == 'order':
+            while abs(angleDiff) > 0.03:
+                robotAngle = math.asin(theta)
+                angleDiff = math.atan((goaly - y) / (goalx- x)) - 2*robotAngle
+                vel.angular.z = 1.5*angleDiff
+                pub.publish(vel)
+            vel.angular.z = 0
 
-        while abs(angleDiff) > 0.03:
-            robotAngle = math.asin(theta)
-            angleDiff = math.atan((goaly - y) / (goalx- x)) - 2*robotAngle
-            vel.angular.z = 1.5*angleDiff
+            while abs(linear) > 0.05:
+
+                linear = 0.5*math.sqrt((goalx - x)**2 + (goaly - y)**2)
+                vel.linear.x = linear
+                pub.publish(vel)
+            vel.linear.x = 0
+            # print(vel.linear.x, vel.angular.z)
             pub.publish(vel)
-        vel.angular.z = 0
-
-        while abs(linear) > 0.05:
-
-            linear = 0.5*math.sqrt((goalx - x)**2 + (goaly - y)**2)
-            vel.linear.x = linear
-            pub.publish(vel)
-        vel.linear.x = 0
-        print(vel.linear.x, vel.angular.z)
-        pub.publish(vel)
-        rate.sleep()
+            rate.sleep()
 
 
 if __name__ == '__main__':
